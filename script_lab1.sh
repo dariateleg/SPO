779db4e1f6229fce0a5870b9091125522a0b5d78
BashKirian <dariateleg@gmail.com>
2019-10-13 13:31:09 +0300

    work with spaces and dots

--- script_lab1.sh
+++ script_lab1.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
-files=$(find "$1" -type f)
-vals=""
-dates=""
-#создаем переменные для хранения длительности видео- аудиофайлов
-for file in $files; do
-	videol=$(ffprobe -i "$file" -sexagesimal -show_entries format=duration -v quiet -of csv="p=0"); 
+$(rm ~/excel.xls) #удаляем файл для записи результата работы скрипта
+title="abs path + name\ttype\tsize(bytes)\tchanged date\tvideo or audio length (h.m.s.ms)\n"
+printf "$title" >> ~/excel.xls #добавляем заголовок для таблицы
+find  "$1" -type f -print0 | while IFS= read -r -d '' file; do  #находим рекурсивно все файлы, используя в качестве разделителя нулевой байт
+	name=$(echo "$file" | sed 's/\.[^.]*$//') #достаем абсолютное имя
+	ext=$(echo "$file" | sed 's/^.*\.//') #достаем расширение 
+	size=$(stat -c '%s' "$file") #достаем размер
+	date=$(stat -c '%z' "$file") #достаем дату изменения файла
+	videol=$(ffprobe -i "$file" -sexagesimal -show_entries format=duration -v quiet -of csv="p=0"); #достаем видео или аудио длину файла
 	if [[ $videol == "" ]]; then
-		videol="0"
+		videol="0" #обнуляем длительность не видео и аудио файлов
 	fi
-	vals=$vals" "$videol;
-	dates=$dates""$(stat -c %z "$file")"|";
+	if [[ "$ext" == "avi" || "$ext" == "mp4" || "$ext" == "mp3" ]]; then 
+		thing=$name"\t"$ext"\t"$size"\t"$date"\t"$videol"\t\n"
+		printf "$thing" >> ~/excel.xls #добавляем в таблицу информацию о видео и аудио файлах
+	else 
+		thing=$name"\t"$ext"\t"$size"\t"$date"\t\n"
+		printf "$thing" >> ~/excel.xls #добавляем в таблицу информацию об остальных файлах
+	fi
 done
-#в цикле проходимся по каждому файлу из заданной директории и записываем длительность в переменную vals
-find $1 -type f -exec stat -c '%n|%s' {} + |
-sed 's/\./|/g' |
-sed 's/,/\./g' |
-awk -F "|" -v a="$vals" -v b="$dates" '
-BEGIN {
-split(a,list," ");
-split(b,dates,"|");
-print "abs path + name\ttype\tsize (bytes)\tchanged date\tvideo or audio length (h.m.s.ms)";
-}
-{
-if ($2=="avi" || $2=="mp4" || $2=="mp3")
-	print $1"\t"$2"\t"$3"\t"dates[NR]"\t"list[NR];
-else
-	print $1"\t"$2"\t"$3"\t"dates[NR]"\t";
-}' > ~/excel.xls
-#с помощью ls получаем информацию о файлах из директории, форматируем, заменяя точки на пробел 
-#(для отделения нового столбца с расширениями файлов), заменяем запятые на точки, в переменную а передаем строку с длительностью, 
-#форматируем строку в лист. После печатаем в конечный xls-файл заголовки для параметров и в зависимости от типа файла выводим все параметры с длительностью или без нее.
-
